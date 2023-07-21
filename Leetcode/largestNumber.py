eca73f7d753021b69440b68b889992e6e0173700
solo2112 <dawitabrham0021@gmail.com>
2023-07-21 09:21:58 +0300

    largest number

--- Leetcode/largestNumber.py
+++ Leetcode/largestNumber.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         for i, n in enumerate(nums):
@@ -11,5 +14,21 @@ class Solution:
             return nums
 
         nums = sorter(nums)
+
+        return str(int("".join(nums)))
+
+
+
+class Solution2:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i,n in enumerate(nums):
+            nums[i] = str(n)
+
+        def comparetor(n1,n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+        nums = sorted(nums,key = cmp_to_key(comparetor))
 
         return str(int("".join(nums)))
