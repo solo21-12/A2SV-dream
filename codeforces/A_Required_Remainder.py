4df80ff7383def475c45609e4ac80b3bed4f122c
solo2112 <dawitabrham0021@gmail.com>
2023-12-23 09:39:33 +0300

    new problem update

---
+++ codeforces/A_Required_Remainder.py
@@ -0,0 +1,13 @@
+
+
+def solver(x, y, n):
+    max_num = 0
+    
+
+    return max_num
+
+
+t = int(input())
+for i in range(t):
+    x, y, n = map(int, input().split(" "))
+    print(solver(x,y,n))
